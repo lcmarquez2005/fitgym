49d81b8b981505fe6b56e3deb6444d4eaf80b2f7
Luis Marquez <lcarlosmarquezstrociak@gmail.com>
2026-05-16 19:33:51 -0600

    fonts weight problems solved

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,22 @@ import RouterApp from './RouterApp';
 import { BrowserRouter } from 'react-router-dom' 
 import './index.css' // <--- IMPORTANTE
 
+// Global font imports
+const FontImports = () => (
+  <style>
+    {`
+      @import url('https://fonts.googleapis.com/css2?family=Bakbak+One&display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
+      
+      .font-bakbak { font-family: 'Bakbak One', sans-serif; }
+      .font-inter { font-family: 'Inter', sans-serif; }
+    `}
+  </style>
+)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <FontImports />
     <BrowserRouter>  
       <RouterApp />
     </BrowserRouter>
